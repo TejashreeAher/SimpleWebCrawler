fab3535c74c13c84fbe16a09d2f22fa1205b2e45
Tejashree Aher <tejashree.aher@myntra.com>
2015-04-13 11:15:45 +0530

    Added code to get list of all cars

--- src/crawler/CarListCrawler.java
+++ src/crawler/CarListCrawler.java
@@ -9,9 +9,13 @@ package crawler;
 import com.mysql.jdbc.StringUtils;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.sf.json.JSONObject;
+import net.sf.json.util.JSONBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -25,6 +29,7 @@ public class CarListCrawler implements BaseCrawler{
     
     private String url;
     private List<String> allCars = new ArrayList<String>();
+    private Map<String, List<String>> makerToModelListMap = new HashMap<String, List<String>>();
     
     public CarListCrawler(String url){
         this.url = url;
@@ -34,8 +39,7 @@ public class CarListCrawler implements BaseCrawler{
         
     }
 
-    @Override
-    public void processPage(String URL) {
+    private void setCarModelsList(String URL){
         try {
             Document doc = Jsoup.connect(URL).get();
             Element carNamesElement = doc.select("div.margin-top15.margin-bottom15").first();
@@ -46,6 +50,14 @@ public class CarListCrawler implements BaseCrawler{
             {
                 if(!element.attr("value").equals("-1")){
                     System.out.println("id: "+ element.attr("value")+":"+element.text());
+                    String id = element.attr("value");
+                    Document modelDoc = Jsoup.connect("http://www.carwale.com/webapi/carmodeldata/GetCarModelsByType/?type=new&makeId="+id).get();
+                    System.out.println("For id: "+ id+", Models are ----------"+modelDoc.text());
+                    //[{"ModelId":444,"ModelName":"Pajero Sport","MaskingName":"pajerosport"}]
+                    String modelsString = modelDoc.text();
+                    
+                    System.out.println("---------------------------------------");
+
                     String car = element.text();
                     if(!StringUtils.isEmptyOrWhitespaceOnly(car)){
                         allCars.add(car);
@@ -53,16 +65,18 @@ public class CarListCrawler implements BaseCrawler{
                 }
             }
             
-            Elements carModels = doc.select("#drpModelNew.margin-left5.leftfloat");
-            System.out.println("Models : "+ carModels.text());
         } catch (IOException ex) {
             Logger.getLogger(CarListCrawler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    @Override
+    public void processPage(String URL) {
+        
+    }
     
     public static void main(String[] args){
         CarListCrawler crawler = new CarListCrawler("http://www.carwale.com/");
-        crawler.processPage(crawler.url);
+        crawler.setCarModelsList(crawler.url);
     }
     
 }
